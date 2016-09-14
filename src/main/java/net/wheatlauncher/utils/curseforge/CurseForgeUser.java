af61e67721af3d5f0f1efe09e86f79861de5a0ac
ci010 <cijhn@hotmail.com>
2016-09-14 20:26:49 +0800

    support curse category; support curse users;

---
+++ src/main/java/net/wheatlauncher/utils/curseforge/CurseForgeUser.java
@@ -0,0 +1,30 @@
+package net.wheatlauncher.utils.curseforge;
+
+import java.util.List;
+
+/**
+ * @author ci010
+ */
+public class CurseForgeUser
+{
+	private String name;
+	private String imageUrl;
+	private List<CurseForgeMinecraftProject> projectCache;
+
+	public CurseForgeUser(String name, String imageUrl)
+	{
+		this.name = name;
+		this.imageUrl = imageUrl;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public String getImageUrl()
+	{
+		return imageUrl;
+	}
+
+}
